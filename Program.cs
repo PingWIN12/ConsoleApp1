e6313466459b79e94c0688bc1fc2f13493268156
PingWIN12 <57216359+PingWIN12@users.noreply.github.com>
2020-06-17 12:19:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Create("J:\\new_file.txt");
+            File.WriteAllText("J:\\new_file.txt", "текст");
+            File.AppendAllText("J:\\new_file.txt", "текст метода AppendAllText ()");
+            File.Delete("J:\\test.txt");
+
+            FileStream file1 = new FileStream("J:test.txt", FileMode.Open);
+
+        }
+    }
+}
